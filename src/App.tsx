8090787d9f7a052e1ca7f1c4344caea44ba4e44e
ljimshitashvili <jimshitashvili4444@gmail.com>
2023-05-07 17:17:14 +0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import GlobalStyle from "./GlobalStyles";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <Container>
+      <GlobalStyle />
+    </Container>
+  );
+}
+
+export default App;
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+`;
