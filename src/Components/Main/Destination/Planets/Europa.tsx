3368e9acb09a0f62561be2bf7170ad5b127f698d
ljimshitashvili <jimshitashvili4444@gmail.com>
2023-05-08 06:53:54 +0400

    Europa

--- src/Components/Main/Destination/Planets/Europa.tsx
+++ src/Components/Main/Destination/Planets/Europa.tsx
@@ -2,24 +2,154 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { styled } from "styled-components";
 
+import EuropaIcon from "../../../../assets/starter-code/assets/destination/image-europa.png";
+
 interface Props {
   setDestPath: (depthPath: string) => void;
 }
 
-export default function Moon({ setDestPath }: Props) {
+export default function Europa({ setDestPath }: Props) {
   const location = useLocation();
   useEffect(() => {
     setDestPath(location.pathname);
   }, [location]);
+
   return (
     <Container>
-      <h1>moon</h1>
+      <h1>
+        <span>01</span>Pick your destination
+      </h1>
+      <img src={EuropaIcon} alt="Europa" />
+      <h2>EUROPA</h2>
+      <p>
+        The smallest of the four Galilean moons orbiting Jupiter, Europa is a
+        winter lover’s dream. With an icy surface, it’s perfect for a bit of ice
+        skating, curling, hockey, or simple relaxation in your snug wintery
+        cabin.
+      </p>
+      <hr />
+      <h3>AVG. DISTANCE</h3>
+      <h4>628 MIL. km</h4>
+      <h5>Est. travel time</h5>
+      <h6>3 years</h6>
     </Container>
   );
 }
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 24px 0;
+  position: relative;
+
   h1 {
-    color: white;
+    font-family: "Barlow Condensed";
+    font-size: 16px;
+    line-height: 19px;
+    font-weight: 400;
+    letter-spacing: 2.7px;
+    color: #ffffff;
+    mix-blend-mode: normal;
+    text-transform: uppercase;
+    display: flex;
+    gap: 18px;
+    align-items: center;
+
+    span {
+      font-family: "Barlow Condensed";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 16px;
+      line-height: 19px;
+      letter-spacing: 2.7px;
+      color: #ffffff;
+      opacity: 0.25;
+    }
+  }
+
+  h2 {
+    font-family: "Bellefair";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 56px;
+    line-height: 64px;
+    text-align: center;
+    color: #ffffff;
+  }
+
+  p {
+    font-family: "Barlow";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 25px;
+    text-align: center;
+    color: #d0d6f9;
+  }
+
+  h3 {
+    font-family: "Barlow Condensed";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 17px;
+    text-align: center;
+    letter-spacing: 2.3625px;
+    text-transform: uppercase;
+    color: #d0d6f9;
+    margin-bottom: 12px;
+  }
+
+  h4 {
+    font-family: "Bellefair";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 28px;
+    line-height: 32px;
+    text-align: center;
+    text-transform: uppercase;
+    color: #ffffff;
+    margin-bottom: 32px;
+  }
+
+  h5 {
+    font-family: "Barlow Condensed";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 17px;
+    text-align: center;
+    letter-spacing: 2.3625px;
+    text-transform: uppercase;
+    color: #d0d6f9;
+    margin-bottom: 12px;
+  }
+
+  h6 {
+    font-family: "Bellefair";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 28px;
+    line-height: 32px;
+    text-align: center;
+    text-transform: uppercase;
+    color: #ffffff;
+  }
+
+  img {
+    width: 170px;
+    height: 170px;
+    margin: 32px 0 74px 0;
+  }
+
+  hr {
+    width: 100%;
+    border: none;
+    height: 1px;
+    background: #383b4b;
+    margin: 32px 0;
   }
 `;
